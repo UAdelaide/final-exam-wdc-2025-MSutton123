bef753ca9b388ce0448c5f438039ff3a55b97801
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 09:27:35 +0000

    [automatic commit] 2025-06-20 09:27:35 UTC 122.151.36.132

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  socketPath: '/var/run/mysqld/'
+  socketPath: '/var/run/mysqld/mysqld.sock',
   host: '127.0.0.1',
   user: 'root',
   database: 'DogWalkService',
