f2af9a864912503302c51d450630c60008461321
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 15:23:46 +0000

    [automatic commit] 2025-06-20 15:23:46 UTC 122.151.36.132

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // make session for user
+    // make session for user using the data from the query
     var first_row = rows[0];
     req.session.user = {
                     user_id: first_row.user_id,
