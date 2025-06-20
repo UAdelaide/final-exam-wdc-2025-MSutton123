5a0c360a92e26b63dd9d37f22787db38fc36af2b
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 03:23:04 +0000

    [automatic commit] 2025-06-20 03:23:04 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -27,16 +27,10 @@ router.get('/walkrequests/open', async (req, res) => {
 // GET requests for /walkers/summary
 router.get('/walkers/summary', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT u.username AS walker_username, COUNT(rate.rating_id) AS total_ratings, AVG(rate.rating) AS average_rating, 
+        SELECT u.username AS walker_username, COUNT(rate.rating_id) AS total_ratings, AVG(rate.rating) AS average_rating,
         FROM Users u
         JOIN WalkRatings rate ON u.user_id = rate.walker_id
-        WHERE u.role = 'walker'
-
-        SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
-        FROM Dogs d
-        JOIN WalkRequests wr ON wr.dog_id = d.dog_id
-        JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status = 'open'
+        WHERE u.role = 'walker' AND 
     `);
     res.json(rows);
 });
