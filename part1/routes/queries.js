f59d845a1ba69d574c7526c4ea65bf31dbc83bf8
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 02:53:43 +0000

    [automatic commit] 2025-06-20 02:53:43 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.router();
 var db = require('../db');
 
+// /dogs GET request
 router.get('/dogs', async (req, res) => {
-
+    const [rows] = db.sql();
 });
