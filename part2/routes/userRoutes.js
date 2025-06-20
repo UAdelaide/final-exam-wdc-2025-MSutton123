f50b1ff551c05d05fee9dc9c5176420423802395
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 09:29:58 +0000

    [automatic commit] 2025-06-20 09:29:58 UTC 122.151.36.132

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -14,8 +14,9 @@ router.get('/', async (req, res) => {
 
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
-  const { username, email, password, role } = req.body;
   console.log("here");
+
+  const { username, email, password, role } = req.body;
   try {
     const [result] = await db.query(`
       INSERT INTO Users (username, email, password_hash, role)
