470375808521438f6101ce70c7e8085cfce6b45a
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 15:17:17 +0000

    [automatic commit] 2025-06-20 15:17:17 UTC 122.151.36.132

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+// make the session
 var session = require('express-session');
 
 app.use(session({
