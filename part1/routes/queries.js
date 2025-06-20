cc93a07056d46a08d9305d083f32a1b9b95ab72e
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 03:14:31 +0000

    [automatic commit] 2025-06-20 03:14:31 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -17,7 +17,7 @@ router.get('/walkrequests/open', async (req, res) => {
     const [rows] = await db.query(`
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, rw.duration_minutes, rw.location, u.username AS owner_username
         FROM Dogs d
-        JOIN WalkRquests wr ON rw.dog_id = d.dog_id
+        JOIN WalkRequests wr ON rw.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status = 'open'
     `);
