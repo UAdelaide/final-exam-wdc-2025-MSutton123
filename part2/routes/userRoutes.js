e8349826e39515095eb51a420d8d1c363f8ef1f3
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 09:32:22 +0000

    [automatic commit] 2025-06-20 09:32:22 UTC 122.151.36.132

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -46,6 +46,7 @@ router.post('/login', async (req, res) => {
     `, [username, password]);
 
     if (rows.length === 0) {
+      console.log("here");
       // return res.status(401).json({ error: 'Invalid credentials' });
     }
 
