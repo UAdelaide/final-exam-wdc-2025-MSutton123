2cb50cb7be5afeed93e41d430a3935fe96559be2
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 09:31:40 +0000

    [automatic commit] 2025-06-20 09:31:40 UTC 122.151.36.132

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  console.log("here");
+  
   const { username, password } = req.body;
 
   try {
