186e1821f3f0796ae0a165425cd7c066a5abf9a7
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 08:54:49 +0000

    [automatic commit] 2025-06-20 08:54:48 UTC 122.151.36.132

--- part2/app.js
+++ part2/app.js
@@ -20,6 +20,7 @@ var session = require('express-session');
 app.use(session({
     secret: 'secret-key',
     resave: false,
+    saveUninitialized: false,
     cookie: {
         secure: false
     }
