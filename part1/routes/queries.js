823c3871d29450ae2fc7b9817c1d8f9898dd8f8b
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 03:09:44 +0000

    [automatic commit] 2025-06-20 03:09:44 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -15,6 +15,7 @@ router.get('/dogs', async (req, res) => {
 // GET requests for /walkrequests/open
 router.get('/walkrequests/open', async (req, res) => {
     const [rows] = await db.query(`
+        SELECT 
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
