a2abbfeffe87fd2ee69ed7a025d2a6774fe175b3
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 07:52:43 +0000

    [automatic commit] 2025-06-20 07:52:43 UTC 122.151.36.132

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-var session = 
+var session = require('express-session');
 
 const app = express();
 
