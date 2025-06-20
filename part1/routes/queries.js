f2b5bdf7e37a2e0becbde2b4b3377afa9bae8c87
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 03:55:07 +0000

    [automatic commit] 2025-06-20 03:55:07 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -5,37 +5,37 @@ var db = require('../db');
 // GET requests for /api/dogs
 router.get('/dogs', async (req, res) => {
     try {
-    const [rows] = await db.query(`
+        const [rows] = await db.query(`
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
     `);
-    res.json(rows);
+        res.json(rows);
     } catch (error) {
-        res.status(501).send("/api/dogs request failed. "+error);
+        res.status(501).send("/api/dogs request failed. " + error);
     }
 });
 
 // GET requests for /walkrequests/open
 router.get('/walkrequests/open', async (req, res) => {
     try {
-    const [rows] = await db.query(`
+        const [rows] = await db.query(`
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
         FROM Dogs d
         JOIN WalkRequests wr ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status = 'open'
     `);
-    res.json(rows);
-} catch (error) {
-    res.status(501).send("/api/walkrequests/open request failed. "+error);
-}
+        res.json(rows);
+    } catch (error) {
+        res.status(501).send("/api/walkrequests/open request failed. " + error);
+    }
 });
 
 // GET requests for /walkers/summary
 router.get('/walkers/summary', async (req, res) => {
     try {
-    const [rows] = await db.query(`
+        const [rows] = await db.query(`
         SELECT u.username AS walker_username,
             COUNT(rate.rating_id) AS total_ratings,
             ROUND(AVG(rate.rating), 1) AS average_rating,
@@ -49,10 +49,10 @@ router.get('/walkers/summary', async (req, res) => {
         WHERE u.role = 'walker'
         GROUP BY u.username
     `);
-    res.json(rows);
-} catch (error) {
-    res.status(500).send("/api/ request failed. "+error);
-}
+        res.json(rows);
+    } catch (error) {
+        res.status(500).send("/api/walkers/summary request failed. " + error);
+    }
 });
 
 module.exports = router;
