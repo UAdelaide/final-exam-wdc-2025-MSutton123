e7972a9e1ad5707cf3e50eb7f3ab23b435085f7b
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 03:19:38 +0000

    [automatic commit] 2025-06-20 03:19:38 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -27,7 +27,7 @@ router.get('/walkrequests/open', async (req, res) => {
 // GET requests for /walkers/summary
 router.get('/walkers/summary', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT u.user_id AS walker_username, AS total_ratings
+        SELECT u.user_id AS walker_username, COUNT() AS total_ratings
         FROM Users s
         WHERE u.role = 'walker'
 
