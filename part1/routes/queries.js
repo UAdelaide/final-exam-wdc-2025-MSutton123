80781b14a56878a4034ac5deb757ec624e5a6f68
MSutton123 <a1851773@adelaide.edu.au>
2025-06-20 02:58:09 +0000

    [automatic commit] 2025-06-20 02:58:09 UTC 122.151.36.132

--- part1/routes/queries.js
+++ part1/routes/queries.js
@@ -8,6 +8,5 @@ router.get('/dogs', async (req, res) => {
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
-        
     `);
 });
